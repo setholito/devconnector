02cc8f7fac4e9ecf8a9146916d2d2ecc68ae3adc
Seth Davis <seth.davis@egov.com>
2018-04-24 10:00:10 -0500

    Update users route and model. Add ability to login.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../../config/keys')
+
+// Load user model
+const User = require('../../models/User')
 
 // @route    GET api/users/test
 // @desc     Tests users routes
@@ -9,5 +16,90 @@ router.get('/test', (req, res) =>
         message: 'Users works.'
     })
 )
+
+// @route    GET api/users/register
+// @desc     Register user
+// @acccess  Public
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({
+                email: 'Email already exists'
+            })
+        } else {
+            const avatar = gravatar.url(req.body.email, {
+                s: '200', // Size
+                r: 'pg', // Rating
+                d: 'mm' // Default
+            })
+
+            const newUser = new User({
+                avatar,
+                email: req.body.email,
+                name: req.body.name,
+                password: req.body.password
+            })
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) {
+                        throw err
+                    }
+
+                    newUser.password = hash
+
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err))
+                })
+            })
+        }
+    })
+})
+
+// @route    GET api/users/login
+// @desc     Login user / Returning JWT token
+// @acccess  Public
+router.post('/login', (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    User.findOne({ email }).then(user => {
+        // Check for user
+        if (!user) {
+            return res.status(404).json({ email: 'User not found' })
+        }
+
+        // Check password
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                // User Matched
+                const payload = {
+                    id: user.id,
+                    name: user.name,
+                    avatar: user.avatar
+                }
+
+                // Sign Token
+                jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    {
+                        expiresIn: 3600
+                    },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: `Bearer ${token}`
+                        })
+                    }
+                )
+            } else {
+                return res.status(400).json({ password: 'Password incorrect' })
+            }
+        })
+    })
+})
 
 module.exports = router
